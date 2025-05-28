cccc2ee701718a4e26409fc78434ee4c94e82d5c
Willie-Conway <hire.willie.conway@gmail.com>
2025-05-28 11:16:11 -0400

    update repo

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,52 @@
-import React from 'react';
+// client\src\components\auth\Login.js
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { login } from '../../redux/actions/authActions';
 
 const Login = () => {
+  const dispatch = useDispatch();
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
+
   return (
     <div>
       <h2>Login Page</h2>
-      {/* Add your login form here */}
+      <form onSubmit={onSubmit}>
+        <div>
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={email}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 };
